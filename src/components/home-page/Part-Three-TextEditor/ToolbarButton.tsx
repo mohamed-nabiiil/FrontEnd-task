dbfae24c1c35afaaf13b069a3b3df896887826c8
Mohamed Nabil <mamadonabil919@gmail.com>
2024-04-04 08:54:32 +0200

    delete prop

--- src/components/home-page/Part-Three-TextEditor/ToolbarButton.tsx
+++ src/components/home-page/Part-Three-TextEditor/ToolbarButton.tsx
@@ -6,20 +6,15 @@ interface ToolbarButtonProps {
   icon: string;
   alt: string;
   onClick?: () => void;
-  isActive?: boolean;
 }
 
 const ToolbarButton: React.FC<ToolbarButtonProps> = ({
   icon,
   alt,
   onClick,
-  isActive,
 }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`${styles.toolbarButton} ${isActive ? styles.active : ""}`}
-    >
+    <button onClick={onClick} className={`${styles.toolbarButton}`}>
       <Image src={icon} alt={alt} width={24} height={24} />
     </button>
   );
